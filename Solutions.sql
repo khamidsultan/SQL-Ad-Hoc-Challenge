3d17be86092cf105bde25f00e9de73ef9dffe929
Abdulkhamid <93081521+khamidsultan@users.noreply.github.com>
2024-06-30 14:22:26 +0500

    Update and rename Solutions.txt to Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -39,7 +39,6 @@ JOIN
 
 # 3. Provide a report with all the unique product counts for each segment and sort them in descending order of product counts. 
 # The final output contains 2 fields, 
-
 # segment 
 # product_count
 
